8d1f901a6fba5680a1c7f87046f00bd8b0d92f29
luolinfeng <luolinfeng@seaskylight.com>
2025-06-28 11:06:41 +0800

    refactor: clean up imports in InstrumentService by removing unused exceptions

--- src/instrument/instrument.service.ts
+++ src/instrument/instrument.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, TreeRepository } from 'typeorm';
 import { Instrument } from './entities/instrument.entity';
